8d466c6ad55f0c5ce8a238c01c97925e45fdcff3
utkarsh-sohane <98829006+utkarsh-sohane@users.noreply.github.com>
2023-12-07 22:36:18 +0530

    Add files via upload

---
+++ PINGPONG/PINGPONG.java
@@ -0,0 +1,11 @@
+package PINGPONG;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PINGPONG {
+    public static void main(String[] args){
+        GameFrame frame = new GameFrame();//instance
+    }
+}
